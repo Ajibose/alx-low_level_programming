b59a2eb42fdb713e831d7262d745ff7e60c6375f
Ajibose <ajiboseibrahim12@gmail.com>
2022-12-14 11:00:17 -0800

    function that adds two integers and returns the result.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Adds two number
+ * @n1: First number to be added
+ * @n2: Second number to be added
+ *
+ * Return: res
+ */
+
+int add(int n1, int n2)
+{
+	int res;
+	res = n1 + n2;
+	return (res);
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,11 @@
+#include "main"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ *
+ * return: 0
+ */
+
+void jack_bauer(void)
+{
+	
