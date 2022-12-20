e17719361ab7d3cccec1d086b3345406661f7f9b
Ajibose <ajiboseibrahim12@gmail.com>
2022-12-20 14:14:36 -0800

    a function that takes a pointer to an int as parameter and updates the value it points to to 98.

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Print all single digit of base 10
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		printf("%d", num);
+		num++;
+	}
+
+	putchar(10);
+	return (0);
+}
