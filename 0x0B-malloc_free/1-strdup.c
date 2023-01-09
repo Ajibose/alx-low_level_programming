19ac6f4715a1b72a60507fef5866f29a81bf77a9
Ajibose <ajiboseibrahim12@gmail.com>
2023-01-09 11:42:07 -0800

    Copies a string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strdup - Returns pointer to a copied string
+ * @str: string to copy
+ *
+ * Return: NULL or pointer to the copied string
+ */
+
+char *_strdup(char *str)
+{
+	int i, j;
+	char *s;
+
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; *(str + i); i++)
+		;
+	s = malloc(sizeof(char) * i);
+	if (s == NULL)
+		return (NULL);
+
+	for (j = 0; j < i; j++)
+		*(s + j) = *(str + j);
+	*(s + i) = *(str + i);
+
+	return (s);
+	free(s);
+}
