ae363be9c8f40b512a26442041491b648d830aa7
Ajibose <ajiboseibrahim12@gmail.com>
2023-01-09 12:19:23 -0800

    Creates an array and initializezs it

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * alloc_grid - creates an array and initializeit to 0
+ * @height: row of the array
+ * @width: column of the array
+ *
+ * Return: NULL or two_array
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **two_arr;
+	int i, j, k;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	two_arr = malloc(sizeof(int *) * height);
+	for (i = 0; i < height; i++)
+	{
+		*(two_arr + i) = malloc(sizeof(int) * width);
+		if (*(two_arr + i) == NULL)
+		{
+			for (k = 0; k < i; k++)
+			{
+				free(*(two_arr + i));
+			}
+			free(two_arr);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+			two_arr[i][j] = 0;
+	}
+	return (two_arr);
+}
