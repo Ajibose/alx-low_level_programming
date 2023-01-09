81c21ded788d726bc7f1a8528276a2da7903ce79
Ajibose <ajiboseibrahim12@gmail.com>
2023-01-09 11:49:37 -0800

    Copies a string

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,7 +14,7 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 		return (NULL);
-	for (i = 0; *(str + i); i++)
+	for (i = 0; *(str + i) != '\0'; i++)
 		;
 	s = malloc(sizeof(*str) * i);
 	if (s == NULL)
