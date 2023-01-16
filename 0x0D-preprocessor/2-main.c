3dd51a442de7f2fbcf4fe0452b64f45c67345242
Ajibose <ajiboseibrahim12@gmail.com>
2023-01-16 06:15:30 -0800

    prints the name of the file it was compiled from

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,8 +2,6 @@
 
 /**
  * main - Prints the name of the file
- * @argv: array of the arguments passed to the program
- * @argc: length of argv
  *
  * Return: 0 on success
  */
@@ -11,5 +9,5 @@
 int main(void)
 {
 	printf("%s\n", __FILE__);
-	return(0);
+	return (0);
 }
