3a2a6468437155caf1b6b53d6af798f26642f2cb
Ajibose <ajiboseibrahim12@gmail.com>
2023-01-16 10:27:05 -0800

    a function that initialize a struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ * init_dog - Initialize the dog struct
+ *
+ * @d: pointer to dog structure
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: Owner of dog
+ *
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
