1361800379204603547dd2bf74043424d76a7142
Ajibose <ajiboseibrahim12@gmail.com>
2023-02-07 09:21:16 -0800

    creates a file and write to it

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 	int uniq, nbyte, len;
 
 	if (filename == NULL)
-		return (0);
+		return (-1);
 
 	uniq = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (uniq == -1)
