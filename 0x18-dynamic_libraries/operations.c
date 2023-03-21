5f90ab748a786cc56ca1115a4643230179e6e6a1
Ajibose <ajiboseibrahim12@gmail.com>
2023-03-21 11:23:22 -0700

    Basic operations(+, -, *, /) library

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,49 @@
+#include "operations.h"
+
+/**
+ * add - add two numbers
+ * @n1: first number
+ * @n2: second number
+ *
+ * Return: sum of two numbers
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
+
+/**
+ * sub - perform a subtraction operation on two numbers
+ * @n1: first number
+ * @n2: second number
+ *
+ * Return: difference of the two numbers
+ */
+int sub(int n1, int n2)
+{
+	return (n1 - n2);
+}
+
+/**
+ * mul - perform a multiplication operation on two numbers
+ * @n1: first number
+ * @n2: second number
+ *
+ * Return: result of multiplication
+ */
+int mul(int n1, int n2)
+{
+	return (n1 * n2);
+}
+
+/**
+ * div - perform a division operation on two numbers
+ * @n1: first number
+ * @n2: second number
+ *
+ * Return: result of division
+ */
+int div(int n1, int n2)
+{
+	return (n1 / n2);
+}
