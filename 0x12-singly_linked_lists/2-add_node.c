c9f5f1271e9af078640a89c7cac524ee0071f92d
Ajibose <ajiboseibrahim12@gmail.com>
2023-04-11 09:13:31 -0700

    adds a new node at the beginning of a linked list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,7 +13,10 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *temp = malloc(sizeof(list_t));
 
 	if (temp == NULL)
+	{
+		free(temp);
 		return (NULL);
+	}
 
 	temp->str = strdup(str);
 	temp->len = strlen(temp->str);
