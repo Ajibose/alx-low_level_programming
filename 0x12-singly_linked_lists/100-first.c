999f48c0c45bdf194047242291a0b8989eeccf16
Ajibose <ajiboseibrahim12@gmail.com>
2023-04-14 08:29:30 -0700

    A function that executes before the main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+/**
+ * preceedingMain - A function that executes before main
+ *
+ * Return: void
+ */
+void __attribute__((constructor)) preceedingMain(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
