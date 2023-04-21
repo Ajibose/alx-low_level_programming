a0952b60bf86c447d7b10089a3ad2717a3827772
Ajibose <ajiboseibrahim12@gmail.com>
2023-04-21 12:50:51 -0700

    Count and returns the number of nodes in a linked list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * listint_len - Count the number of nodes in a list
+ * @h: Pointer to the first node of a list
+ *
+ * Return: no of odes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t nodes  = 0;
+
+	while (h != NULL)
+	{
+		nodes++;
+		h = h->next;
+	}
+	return nodes;
+}
