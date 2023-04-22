687273962101c1089d3f90e3fe03da27838c1ee9
Ajibose <ajiboseibrahim12@gmail.com>
2023-04-22 03:11:28 -0700

    Frees a linked list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,11 +10,14 @@ void free_listint2(listint_t **head)
 	listint_t *temp;
 	listint_t *ptr = *head;
 
-	while (ptr != NULL)
+	if (ptr != NULL)
 	{
-		temp = ptr;
-		ptr = ptr->next;
-		free(temp);
+		while (ptr != NULL)
+		{
+			temp = ptr;
+			ptr = ptr->next;
+			free(temp);
+		}
+		head = NULL;
 	}
-	head = NULL;
 }
