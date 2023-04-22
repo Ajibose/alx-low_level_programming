41684211f46c7b106fcd8a11ccec7f4f06474f82
Ajibose <ajiboseibrahim12@gmail.com>
2023-04-22 07:37:50 -0700

    Compute the sum of data in a linked list

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,4 +1,11 @@
 #include "lists.h"
+
+/**
+ * sum_listint - Compute te sum of data(n) in a linked list
+ * @head: Pointer to the head node
+ *
+ * Return: Sum of data(n)
+ */
 int sum_listint(listint_t *head)
 {
 	int sum = 0;
