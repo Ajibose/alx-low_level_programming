2b77ee7c42c4b90a8c32cd92bc3e825b62cd0ee6
Ibrahim Ajibose <ajiboseibrahim12@gmail.com>
2023-07-08 00:44:17 -0700

    Task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,27 +7,27 @@
  */
 void print_binary(unsigned long int n)
 {
-        unsigned long int res = n;
-        int bcount = 0, i, bit;
+	unsigned long int res = n;
+	int bcount = 0, i, bit;
 
-        if (n == 0)
-        {
-                putchar('0');
-                return;
-        }
+	if (n == 0)
+	{
+		putchar('0');
+		return;
+	}
 
-        while (res > 0)
-        {
-                bcount++;
-                res = res >> 1;
-        }
+	while (res > 0)
+	{
+		bcount++;
+		res = res >> 1;
+	}
 
-        for (i = bcount - 1; i >= 0; i--)
-        {
-                bit = (n >> i) & 1;
-                if (bit == 1)
-                        _putchar('1');
-                else
-                        _putchar('0');
-        }
+	for (i = bcount - 1; i >= 0; i--)
+	{
+		bit = (n >> i) & 1;
+		if (bit == 1)
+			_putchar('1');
+		else
+			_putchar('0');
+	}
 }
