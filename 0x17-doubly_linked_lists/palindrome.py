6871d8d084a3f75028a7a82bc585c08e239664d3
Ibrahim Ajibose <ajiboseibrahim12@gmail.com>
2023-08-17 19:59:37 +0000

    Advanced task10. Palindromes

--- 0x17-doubly_linked_lists/palindrome.py
+++ 0x17-doubly_linked_lists/palindrome.py
@@ -1,29 +1,46 @@
 #!/usr/bin/python3
-import sys
 
-if len(sys.argv) != 2:
-    print("Usage: ./palindrome <digits>")
-    sys.exit(1)
-
-def check_palindrome(num):
-    reverse_num = 0
-    while num != 0:
-        reverse_num = (reverse_num * 10) + (num % 10)
-
-    if num == reverse_num:
-        return 1
-    return (-1)
-
-digit = sys.argv[1]
-upper_num = pow(10, int(digit)) - 1
-base_num = 1 + int((upper_num / 10))
-palin = 0
-
-for i in range(base_num, upper_num + 1):
-    for j in range(base_num, upper_num + 1):
-        number = i * j
-        i = check_palindrome(number)
-        if i == 1:
-            palin = number
-
-print("{:d}".format(palin))
+def larrgestPalindrome(n):
+ 
+    upper_limit = (10**(n))-1
+      
+    # largest number of n-1 digit.
+    # One plus this number
+    # is lower limit which is
+    # product of two numbers.
+    lower_limit = 1 + upper_limit//10
+  
+    max_product = 0 # Initialize result
+    for i in range(upper_limit,lower_limit-1, -1):
+     
+        for j in range(i,lower_limit-1,-1):
+         
+            # calculating product of
+            # two n-digit numbers
+            product = i * j
+            if (product < max_product):
+                break
+            number = product
+            reverse = 0
+  
+            # calculating reverse of
+            # product to check
+            # whether it is palindrome or not
+            while (number != 0):
+             
+                reverse = reverse * 10 + number % 10
+                number =number // 10
+             
+  
+             # update new product if exist and if
+             # greater than previous one
+            if (product == reverse and product > max_product):
+                max_product = product
+         
+     
+    return max_product
+ 
+# Driver code
+ 
+n = 3
+print(larrgestPalindrome(n))
