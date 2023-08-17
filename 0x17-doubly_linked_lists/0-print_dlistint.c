dc448170a96aed316bd7aa11292853c79cb48d71
Ibrahim Ajibose <ajiboseibrahim12@gmail.com>
2023-08-17 14:28:16 +0000

    Task0. Print list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * print_dlistint - count the number of nodes in a linked list
+ * @h: pointer to the head node
+ *
+ * Return: size of the linked list
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t i = 0;
