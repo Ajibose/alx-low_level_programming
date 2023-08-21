4b1cef4596cb4886eff6129670c8564af1c38cac
Ibrahim Ajibose <ajiboseibrahim12@gmail.com>
2023-08-21 17:16:43 +0000

    Advanced task2

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,62 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+
+/**
+ * sub - perform a subtraction operation on two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: difference of the two numbers
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - perform a multiplication operation on two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: result of multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - perform a division operation on two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: result of division
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - perform a remainder division
+ * @a: first number
+ * @b: second number
+ *
+ * Return: remainder of division
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
