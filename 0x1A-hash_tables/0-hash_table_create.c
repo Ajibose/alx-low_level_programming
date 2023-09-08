5e0cf0e81ab44ff16061ed90d57370a302f16f8c
Ibrahim Ajibose <ajiboseibrahim12@gmail.com>
2023-09-08 16:20:41 +0000

    Task3. >>> ht['betty'] = 'cool'

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,6 +8,7 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	unsigned long int i; 
 	hash_table_t *table;
 
 	table = malloc(sizeof(hash_table_t));
@@ -25,6 +26,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 		free(table);
 		return (NULL);
 	}
+
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
 
 	return (table);
 }
---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,55 @@
+#include "hash_tables.h"
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *item, *head;
+
+	item = create_item(key, value);
+	index = key_index((unsigned char *)key, ht->size);
+
+	if (index > ht->size)
+	{
+		free(item);
+		return (0);
+	}
+
+	if (ht->array[index] == NULL)
+	{
+		ht->array[index] = item;
+	}
+	else
+	{
+		head = ht->array[index];
+		while (head != NULL)
+		{
+			if (strcmp(head->key, key) == 0)
+			{
+				head->value = strdup(value);
+				return (1);
+			}
+			head = head->next;
+		}
+		head = ht->array[index];
+		item->next = head->next;
+		ht->array[index] = item;
+	}
+	return (1);
+}
+
+
+hash_node_t *create_item(const char *key, const char *value)
+{
+	hash_node_t *item = malloc(sizeof(hash_node_t));
+	if (item == NULL)
+	{
+		free(item);
+		return (0);
+	}
+
+	item->key = strdup(key);
+	item->value = strdup(value);
+	item->next = NULL;
+
+	return (item);
+}
