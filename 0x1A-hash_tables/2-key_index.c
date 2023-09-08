c863b2c7857cf561e4676e44b876b0511306ce5e
Ibrahim Ajibose <ajiboseibrahim12@gmail.com>
2023-09-08 11:39:03 +0000

    Task2. key -> index

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,5 +1,11 @@
 #include "hash_tables.h"
 
+/**
+ * key_index - compute the index to store the item in the array
+ * @size: Size of the hash table array
+ *
+ * Return: index to store the key/value pair
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int index;
